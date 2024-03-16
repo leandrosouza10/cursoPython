9ceb9d509f29ed891b546a998e64c9a24298329d
Leandro Souza <souzaleandro79@gmail.com>
2024-03-15 22:48:42 -0300

    Alteração na msg de saída

--- cursoPython/ex003.py
+++ cursoPython/ex003.py
@@ -1,4 +1,4 @@
-valor1 = int (input('Digite um valor: '));
-valor2 = int (input('Digite outro valor: '));
+valor1 = int(input('Digite um valor: '));
+valor2 = int(input('Digite outro valor: '));
 soma = valor1 + valor2;
-print('A soma entre',valor1,'e',valor2,'é igual a', soma);
+print('A soma entre {} e {} é igual a {}!'.format(valor1,valor2,soma));
